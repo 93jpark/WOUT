4bfae4728571050d56a3a747aeef9ad3bfd1a0a0
Joonwoo Park <93jpark@gmail.com>
2020-07-09 22:24:47 +0900

    create project

---
+++ WOUT/WOUT/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WOUT
+//
+//  Created by Joonwoo Park on 2020/07/09.
+//  Copyright © 2020 Joonwoo Park. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
